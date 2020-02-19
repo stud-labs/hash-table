736794dab7143ca83ecc28d74cdf2dccad5f3f69
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-19 09:11:16 +0800

    Added constructor for hash table

--- hash.c
+++ hash.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 // tortoisegit
 // github...
@@ -10,6 +11,14 @@ typedef {
   char ** t;  // Table itself.
 } hashtable_t;
 
+hashtable_t * new_hashtable(size_t size) {
+  hashtable_t * ht = (hashtable_t *) malloc(sizeof(hashtable_t));
+  ht->size = size;
+  ht->len = 0;
+  ht->t = (char **) malloc(size*sizeof(char *));
+
+  return ht;
+}
 
 int main() {
     printf("Hello, World!\n");
